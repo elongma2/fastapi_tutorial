a15686e2f9285950dc722b8523e13f3e04ae22bc
Joshua <joshuablazer72@gmail.com>
2024-11-24 22:07:45 +0800

    added procfile

--- app/main.py
+++ app/main.py
@@ -19,6 +19,7 @@ app.add_middleware(
 """ my_posts=[{"title":"title of post 1","content":"content of post 1","id":1},
           {"title":"favorite foods","content":"I like pizza","id":2}]
  """
+ 
 # router 
 app.include_router(post.router)
 app.include_router(user.router)
